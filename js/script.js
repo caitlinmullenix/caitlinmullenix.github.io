2285963c6a2d674cb96e25939c9e47db6a4a2186
caitlinmullenix <camu4229@colorado.edu>
2022-04-04 18:44:00 -0600

    assignment11

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
-const para = document.querySelector('p');
+/* const para = document.querySelector('p');
 
 para.addEventListener('click', updateName);
 
 function updateName () {
   const name = prompt('Enter a new name');
   para.textContent = `Player 1: S{name}`;
+} */
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+
 }
