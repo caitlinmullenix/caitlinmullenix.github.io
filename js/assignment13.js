1c63062d4a8b7e231bf1035d1046767f470a9da7
caitlinmullenix <camu4229@colorado.edu>
2022-04-17 19:36:36 -0600

    assignment 13 complete

--- js/assignment13.js
+++ js/assignment13.js
@@ -16,17 +16,53 @@ randomize.addEventListener('click', result);
 
 function result() {
 
+  // step 1
+  var newStory = storyText;
+
+  // step 2
+  var itemX = randomValueFromArray(insertX);
+  var itemY = randomValueFromArray(insertY);
+  var itemZ = randomValueFromArray(insertZ);
+
+  // step 3
+  // replace the variables
+  newStory = newStory.replace(":insertx:", itemX);
+  newStory = newStory.replace(":insertx:", itemX);
+  newStory = newStory.replace(":inserty:", itemY);
+  newStory = newStory.replace(":insertz:", itemZ);
+
+
   if(customName.value !== '') {
     const name = customName.value;
 
+    // step 4
+    newStory = newStory.replace("Bob", name);
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
 
+    // step 5
+
+    // converting fahrenheit to celcius
+    // (32°F − 32) × 5/9 = 0°C
+    const temperature =  Math.round(
+      (94-32) * 5/9
+    );
+    newStory = newStory.replace("94 fahrenheit", `${temperature} celcius`)
+
+    // converting pounds to stone
+    // stones = pounds/14
+    const weight = Math.round(
+      300/14
+    );
+    newStory = newStory.replace("300 pounds", `${weight} stone`)
   }
 
-  story.textContent = ;
+  // step 6
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
+
+function test() {
+  alert("hello")
+};
